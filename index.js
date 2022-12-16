44678cceaa399402674781d7f8948e79ab75977e
Benson <benson.kiai86@gmail.com>
2022-12-16 18:55:40 +0300

    JS Test3

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function upcase() {
-    let text = document.getElementById("converter").innerHTML;
-    document.getElementById("converter").innerHTML = text.toUpperCase();
+    let text = document.getElementById("converter").value;
+    document.getElementById("converter").value = text.toUpperCase();
     return text;
   }
   function lowcase() {
+    let text = document.getElementById("converter").value;
+    document.getElementById("converter").value = text.toLowerCase();
+    return text;
+  } 
+  function propcase() {
     let text = document.getElementById("converter").innerHTML;
-    document.getElementById("converter").innerHTML = text.toLowerCase();
+    document.getElementById("converter").innerHTML = text.toProperCase();
     return text;
   } 
