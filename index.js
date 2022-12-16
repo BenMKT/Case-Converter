f31aaab4aebac0873a5c297861780e63f018c7c1
Benson <benson.kiai86@gmail.com>
2022-12-16 18:30:28 +0300

    JS Test button2

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function myFunction() {
+function upcase() {
     let text = document.getElementById("converter").innerHTML;
     document.getElementById("converter").innerHTML = text.toUpperCase();
     return text;
   }
+  function lowcase() {
+    let text = document.getElementById("converter").innerHTML;
+    document.getElementById("converter").innerHTML = text.toLowerCase();
+    return text;
+  } 
