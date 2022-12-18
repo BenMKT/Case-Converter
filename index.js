2ab93475ae8c419589604d7a319d08dc2ac31b44
Benson <benson.kiai86@gmail.com>
2022-12-19 01:50:41 +0300

    added save button

--- index.js
+++ index.js
@@ -15,3 +15,21 @@
     document.getElementById('sentence-case').addEventListener('click', function() {
       text.value = text.value.toLowerCase().split('. ').map(value => value[0].toUpperCase() + value.slice(1)).join('. ');
     });
+
+    function download(filename, text) {
+      let element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(document.querySelector(".textArea").value));
+      element.setAttribute('download', "hello.txt");
+  
+      element.style.display = 'none';
+      document.body.appendChild(element);
+  
+      element.click();
+  
+      document.body.removeChild(element);
+  }
+  
+  download("hello.txt","This is the content of my file :)");
+  
+  document.getElementById("save-text-file").addEventListener("click", download);
+  
